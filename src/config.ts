ca547cbfd24d601a6bfb74a91a80ec4ea97ec992
Rickard Gimerstedt <rickard@gimerstedt.se>
2021-09-19 21:10:31 +0200

    using DB_URL env var in travis

--- src/config.ts
+++ src/config.ts
@@ -4,19 +4,12 @@ function config() {
   dotenv.config()
   const e = process.env
 
-  console.table({
-    NODE_ENV: e.NODE_ENV,
-    CI: e.CI,
-  })
-
   return {
     nodeEnv: e.NODE_ENV,
     port: e.PORT ?? 1234,
     dbUrl:
-      e.NODE_ENV === 'test'
-        ? e.CI
-          ? `mongodb://user:pass@localhost:27017`
-          : `mongodb://user:pass@localhost:27017`
+      e.NODE_ENV === 'test' && !e.CI
+        ? `mongodb://user:pass@localhost:27017`
         : e.DB_URL,
     dbName: e.DB_NAME ?? 'editor',
     dbCollectionName: e.DB_COLL_NAME ?? 'documents',
